4a3b8639fceab757d1f6bdc681032c4ad6b6e5b5
jos3lo89 <gothangel21@hotmail.com>
2025-01-06 12:10:17 -0500

    cpp config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,7 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- cpp config
+vim.cmd("autocmd filetype cpp nnoremap <F9> :w <bar> !g++ -std=c++17 % -o %:p:r<CR>")
+vim.cmd("autocmd filetype cpp nnoremap <F10> :sp <CR> :term %:p:r<CR>")
